29d8ce6046fb5dd94f95a3e07c72466b2aac02d2
Breno Guerra <breno.guerra@tindin.com.br>
2021-06-11 02:01:56 -0300

    feat: middleware to verify if user is admin

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import { UserController } from '../controllers'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import mustBeAdmin from '../middlewares/mustBeAdmin'
 
 const usersRouter = Router()
 
-usersRouter.post('/', UserController.store)
+usersRouter.post('/', ensureAuthenticated, mustBeAdmin, UserController.store)
 
 export default usersRouter
